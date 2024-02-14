97d1b60370d25884f158af04681ece6fd1339635
david-patino <davidpatmal13@gmail.com>
2024-02-14 11:05:53 +0100

    navBar funcional

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,20 +6,19 @@ import { Slide2Component } from './slide2/slide2.component.js';
 import { Slide3Component } from './slide3/slide3.component';
 import { LoginComponent } from './login/login.component';
 import { EleccionComponent } from './eleccion/eleccion.component';
-
+import { HomeComponent } from './home/home.component';
+import { GaleriaComponent } from './Pages/galeria/galeria.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo:'portada', pathMatch:'full' },
-    {path: 'slide1', component:Slide1Component},
-    {path: 'slide2', component:Slide2Component},
-    {path:'slide3', component:Slide3Component},
-    {path: 'login', component:LoginComponent},
-    {path: 'eleccion', component:EleccionComponent},
-    {path:'portada', component:PortadaComponent}
+  { path: '', redirectTo: 'portada', pathMatch: 'full' },
+  { path: 'slide1', component: Slide1Component },
+  { path: 'slide2', component: Slide2Component },
+  { path: 'slide3', component: Slide3Component },
+  { path: 'login', component: LoginComponent },
+  { path: 'eleccion', component: EleccionComponent },
+  { path: 'portada', component: PortadaComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'galeria', component: GaleriaComponent },
 ];
 
-
-
 export class AppRoutingModule {}
-
-
