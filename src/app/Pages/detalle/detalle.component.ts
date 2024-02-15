c6d89e46de28c24f60df4a2050f2fd19c81fdaff
leonardoflorez10 <leo.fabio.florez@bootcamp-upgrade.com>
2024-02-15 19:44:32 +0100

    botones

--- src/app/Pages/detalle/detalle.component.ts
+++ src/app/Pages/detalle/detalle.component.ts
@@ -4,10 +4,11 @@ import { MatIconModule } from '@angular/material/icon'
 import { ApiService } from '../../servicios/api.service';
 import { ActivatedRoute } from '@angular/router';
 
+
 @Component({
   selector: 'app-detalle',
   standalone: true,
-  imports: [MatTabsModule, MatIconModule],
+  imports: [MatTabsModule, MatIconModule,],
   templateUrl: './detalle.component.html',
   styleUrl: './detalle.component.scss'
 })
@@ -19,14 +20,33 @@ export class DetalleComponent {
     this.rutaActivada.paramMap.subscribe(params => {
      this.id = Number(params.get("id"))
      console.log(Number(params.get("id")));
-
     })
 
     this.servicio.getAnimalbyId(this.id).subscribe((data:any)=>{
       console.log(data);
-      
     }
     )
   }
+
+  handleShareClick() {
+    if (navigator.share) {
+      navigator.share({
+        title: 'Título de la información compartida',
+        text: 'Texto descriptivo de la información compartida',
+        url: 'URL de la información compartida',
+      })
+        .then(() => console.log('Información compartida con éxito'))
+        .catch((error) => console.error('Error al intentar compartir:', error));
+    } else {
+      console.log('La API de Web Share no está disponible en este navegador.');
+    }
+  }
+
+  handleLikeClick() {
+    // Lógica para manejar el clic en el botón "Me gusta"
+    console.log('¡Me gusta!');
+    // Puedes agregar aquí la lógica adicional, como enviar una solicitud al servidor, actualizar la interfaz de usuario, etc.
+  }
+
 }
 
