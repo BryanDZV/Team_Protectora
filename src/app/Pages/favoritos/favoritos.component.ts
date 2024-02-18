c06aea65453d994c08b5a9761244072a2c8ec62f
BryanDZV <bryan.dweb@gmail.com>
2024-02-18 15:51:11 +0100

    fav

--- src/app/Pages/favoritos/favoritos.component.ts
+++ src/app/Pages/favoritos/favoritos.component.ts
@@ -2,11 +2,12 @@ import { Component,} from '@angular/core';
 import Animal from '../../../../animal.interface';
 import { RouterLink } from '@angular/router';
 import { ApiService } from '../../servicios/api.service';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-favoritos',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,MatIconModule],
   templateUrl: './favoritos.component.html',
   styleUrl: './favoritos.component.scss'
 })
